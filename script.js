0cf9adeb6ccc3f0bebe0cbd828f33e9556fccb60
Kamron <k1367259@gmail.com>
2025-05-24 17:21:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let names = [
+  `Yahya`,
+  `Komron`,
+  `Abdulaziz`,
+  `Akromjon`,
+  `Jafar`
+]
+
+let N = []
+let n = []
+
+for (nemases of names) {
+  if (nemases.includes(`n`)) {
+    N.push(nemases)
+
+
+  } else {
+    n.push(nemases)
+
+
+  }
+}
+console.log(N);
+console.log(n);
